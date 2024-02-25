f4ef21bd5bbbe2ee5415638c8ca68d408b478de4
San-Tri <1473024@senati.pe>
2024-02-25 11:34:41 -0500

    masDiseño

--- script.js
+++ script.js
@@ -12,4 +12,30 @@ document.addEventListener("DOMContentLoaded", function() {
         formulario.style.display = (formulario.style.display === "block") ? "none" : "block";
         overlay.style.display = (overlay.style.display === "block") ? "none" : "block";
     }
+
+    // Nueva función para agregar productos al carrito
+    function agregarAlCarrito(nombreProducto) {
+        carrito.push(nombreProducto);
+        actualizarContadorCarrito();
+    }
+
+    // Nueva función para actualizar el contador del carrito
+    function actualizarContadorCarrito() {
+        contadorCarrito.textContent = carrito.length;
+    }
+
+    // Nueva función para mostrar el carrito al hacer clic en el icono
+    function mostrarCarrito() {
+        // Puedes implementar la lógica para mostrar el carrito aquí
+        console.log("Productos en el carrito:", carrito);
+    }
+
+    // Eventos para agregar productos al carrito
+    var botonesAgregarCarrito = document.querySelectorAll('.producto button');
+    botonesAgregarCarrito.forEach(function(boton) {
+        boton.addEventListener('click', function() {
+            var nombreProducto = this.parentNode.querySelector('h2').textContent;
+            agregarAlCarrito(nombreProducto);
+        });
+    });
 });
